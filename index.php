dd51f1c11568f108f09cbd6e5b00778e5530eeb7
Maria Clara Gomes <maria.2022315574@aluno.iffar.edu.br>
2024-12-09 11:31:04 -0300

    dia 09/12/2024

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Dia Geek</title>
+    <link rel="shortcut icon" type="image/png" href="fotos/geek.png" />
+    <link rel="stylesheet" href="css/materialize.css">
+    <link rel="stylesheet" href="css/style.css"> <!-- Adicione o arquivo CSS customizado aqui -->
+</head>
+
+<body>
+    <!-- Barra de Navegação -->
+    <nav class="navbar orange">
+        <div class="nav-wrapper">
+            <a href="index.php" class="brand-logo"> <img src="fotos/DIA G K.png" alt="logos"> </a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="fotos/diaGeek.png">
+                        <span class="card-title">Sala 109</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="page-footer color #24b80b orange">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text"> <img src="fotos/DIA G K.png"> </h5>
+                <p class="grey-text text-lighten-4"> Projeto Pratico Integrado (PPI) da turma INFO31 - 2024</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text"> <img src="fotos/logoIFF.png"> </h5>
+                <ul>
+                <li><a class="grey-text text-lighten-3" href="#!">Instagram IFFar</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Instagram INFO31</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Instagem Dia Geek</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2024 Insitituto Federal Farroupilha - Campus Uruguaiana
+            <a class="grey-text text-lighten-4 right" href="#!">Desenvolvedores: Bruno de Lima Jorge & Maria Clara B. R. Dedeco Gomes </a>
+            </div>
+          </div>
+</footer>
+
+
+    <!-- Importando o Materialize JavaScript e o JQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="js/script.js"></script> <!-- Adicione o arquivo JS customizado aqui -->
+</body>
+
+</html>
