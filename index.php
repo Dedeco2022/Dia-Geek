3ec7c6bbade0fde28dc1090347f29f8b49248b4a
Maria Clara Gomes <maria.2022315574@aluno.iffar.edu.br>
2024-12-19 15:45:02 -0300

    19/12/2024

--- index.php
+++ index.php
@@ -10,11 +10,10 @@
   <link rel="stylesheet" href="css/style.css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-  <!-- CSS customizado -->
   <style>
     body {
       position: relative;
-      background-color: rgb(205, 205, 205);
+      background-color: #d4d4d4;
       height: 100%;
       margin-top: auto;
       display: flex;
@@ -54,6 +53,7 @@
     .texto {
       color: black;
     }
+
   </style>
 
 </head>
@@ -74,68 +74,135 @@
     <h1>Informações do evento:</h1>
     <h4> <a class="btn-floating light green"><i class="material-icons">location_on</i></a> Local: Instituto Federal Farroupilha - Campus Uruguaiana</h4>
     <h4> <a class="btn-floating light green"><i class="material-icons">date_range</i></a> Dia: 25/01/2025</h4>
-    <h1>Atrações:</h1>
+    <h1>Atrações:</h1><br>
   </div>
 
 
   <div class="container">
     <div class="carousel">
-      <div class="carousel-item"><img src="fotos/Just-Dance.jpg"><span class="card-title">Just Dance</span></div>
-      <div class="carousel-item"><img src="fotos/"><span class="card-title">Twister</span></div>
-      <div class="carousel-item"><img src="fotos/"><span class="card-title">CS 1.6 </span></div>
-      <div class="carousel-item"><img src="fotos/"><span class="card-title">Exposição de software opensource</span></div>
-      <div class="carousel-item"><img src="fotos/"><span class="card-title">Jogo de Luta</span></div>
-      <div class="carousel-item"><img src="fotos/MortalKombat.jpg"><span class="card-title">Mortal Kombat 11</span></div>
-      <div class="carousel-item"><img src="fotos/Animes.jpg"><span class="card-title">Animes</span></div>
-      <div class="carousel-item"><img src="fotos/"><span class="card-title">Palestra</span></div>
-      <div class="carousel-item"><img src="fotos/"><span class="card-title">Quiz</span></div>
-      <div class="carousel-item"><img src="fotos/JogosTabuleiro.jpeg"><span class="card-title">Jogos de tabuleiro</span></div>
-      <div class="carousel-item"><img src="fotos/FreeFire.jpg"><span class="card-title">Free Fire</span></div>
-      <div class="carousel-item"><img src="fotos/Cosplays.jpg"><span class="card-title">Concurso de Cosplay</span></div>
+      <div class="carousel-item"><img src="fotos/justDance.png"><span class="card-title">Just Dance</span></div>
+      <div class="carousel-item"><img src="fotos/twister.png"><span class="card-title">Twister</span></div>
+      <div class="carousel-item"><img src="fotos/cs.png"><span class="card-title">Campeonato de Counter Strike 1.6</span></div>
+      <div class="carousel-item"><img src="fotos/exposicao.png"><span class="card-title">Exposição de software opensource</span></div>
+      <div class="carousel-item"><img src="fotos/MK9.png"><span class="card-title">Campeonato de MortalKombat 9</span></div>
+      <div class="carousel-item"><img src="fotos/MK11.png"><span class="card-title">Campeonato de MortalKombat 11</span></div>
+      <div class="carousel-item"><img src="fotos/RPG.png"><span class="card-title">RPG</span></div>
+      <div class="carousel-item"><img src="fotos/anime.png"><span class="card-title">Animes</span></div>
+      <div class="carousel-item"><img src="fotos/palestra.png"><span class="card-title">Palestra</span></div>
+      <div class="carousel-item"><img src="fotos/quiz.png"><span class="card-title">Quiz</span></div>
+      <div class="carousel-item"><img src="fotos/tabuleiro.png"><span class="card-title">Jogos de tabuleiro</span></div>
+      <div class="carousel-item"><img src="fotos/desenho.png"><span class="card-title">Concurso de desenho?</span></div>
+      <div class="carousel-item"><img src="fotos/dragonboll.png"><span class="card-title">Campeonato de Dragon Ball Budokai Tenkaichi 3</span></div>
+      <div class="carousel-item"><img src="fotos/freefire.png"><span class="card-title">Campeonato de FreeFire</span></div>
+      <div class="carousel-item"><img src="fotos/cosplay.png"><span class="card-title">Concurso de Cosplay</span></div>
+      <div class="carousel-item"><img src="fotos/fifa20.png"><span class="card-title">FIFA 20</span></div>
     </div>
   </div>
 
   <div class="container">
-    <h1> Competições </h1><br><br>
+    <h1> Campeonatos </h1><br><br>
 
     <div class="row">
-    <div class="col s12 m3">
-      <div class="card">
-        <div class="card-image">
-          <img src="images/sample-1.jpg">
-          <span class="card-title">Card Title</span>
+
+
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="fotos/">
+            <span class="card-title">Campeonato de Counter Strike 1.6</span>
+          </div>
+          <div class="card-content">
+            <p>Sala: Laboratório 2<br>
+              Horário: 08:00</p>
+          </div>
+          <div class="card-action">
+            <a href="https://forms.gle/rBDZ2u8bR1HHepKb9">Inscreva-se!</a>
+          </div>
         </div>
-        <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
+      </div>
+
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="fotos/">
+            <span class="card-title">Campeonato de Mortal Kombat 9</span>
+          </div>
+          <div class="card-content">
+            <p>Sala: 203 <br>
+              Horário: 08:00</p>
+          </div>
+          <div class="card-action">
+            <a href="https://forms.gle/4E1Vz8BMqv3kHX9G7">Inscreva-se!</a>
+          </div>
         </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
+      </div>
+
+
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="fotos/">
+            <span class="card-title">Campeonato de MortalKombat 11</span>
+          </div>
+          <div class="card-content">
+            <p>Sala: 203<br>
+              Horário: 08:00 <br> </p>
+          </div>
+          <div class="card-action">
+            <a href="https://forms.gle/USXtnKekYfKTRa3E7">Inscreva-se!</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="fotos/">
+            <span class="card-title">Campeonato de Dragon Ball Budokai Tenkaichi 3</span>
+          </div>
+          <div class="card-content">
+            <p>Sala: 304<br>
+              Horário: 09:00<br> </p>
+          </div>
+          <div class="card-action">
+            <a href="https://forms.gle/USXtnKekYfKTRa3E7">Inscreva-se!</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="fotos/">
+            <span class="card-title">Campeonato de Free Fire</span>
+          </div>
+          <div class="card-content">
+            <p>Sala: 307 <br>
+              Horário: 09:00</p>
+          </div>
+          <div class="card-action">
+            <a href="https://forms.gle/SgQXy9ro2ZMyK34S8">Inscreva-se!</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="fotos/">
+            <span class="card-title">Concurso de Cosplay</span>
+          </div>
+          <div class="card-content">
+            <p>Sala: Área coberta/Refeitório<br>
+              Horário: 10:00</p>
+          </div>
+          <div class="card-action">
+            <a href="https://forms.gle/JH6hzBn5HXSCRxAp8">Inscreva-se!</a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-
-  <div class="row">
-    <div class="col s12 m3">
-      <div class="card">
-        <div class="card-image">
-          <img src="images/sample-1.jpg">
-          <span class="card-title">Card Title</span>
-        </div>
-        <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
-        </div>
-      </div>
-    </div>
-  </div>
-    
-
-    
   </div>
 
 
